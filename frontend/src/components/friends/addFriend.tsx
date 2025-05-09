15accf2a5d119fc0db62f7c90f65d15a4ce64728
irisvanmelsen <irisvanmelsen@hotmail.com>
2025-05-09 19:36:59 +0200

    removed alert

--- frontend/src/components/friends/addFriend.tsx
+++ frontend/src/components/friends/addFriend.tsx
@@ -29,7 +29,7 @@ const AddFriend: React.FC<AddFriendProps> = () => {
 		  });
 	
 		  if (response.ok) {
-			alert('Friend added successfully!');
+			// alert('Friend added successfully!');
 			setFriend('');
 			setErrorMessage(null)
 			window.location.reload();
