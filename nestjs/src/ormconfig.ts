501bb22c89757a9b589b113ac25448a9b9b590c9
irisvanmelsen <irisvanmelsen@hotmail.com>
2025-05-09 16:46:29 +0200

    volume overwrite fix

--- nestjs/src/ormconfig.ts
+++ nestjs/src/ormconfig.ts
@@ -8,13 +8,13 @@ import { UserConversation } from './conversations/entities';
 import { ChatGameInvite } from './conversations/entities/chat-game-invite.entity';
 
 
-console.log('--- ENV VARIABLES ---');
-console.log('POSTGRES_HOST:', process.env.POSTGRES_HOST);
-console.log('POSTGRES_PORT:', process.env.POSTGRES_PORT);
-console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
-console.log('POSTGRES_PASSWORD:', typeof process.env.POSTGRES_PASSWORD, process.env.POSTGRES_PASSWORD);
-console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
-console.log('----------------------');
+// console.log('--- ENV VARIABLES ---');
+// console.log('POSTGRES_HOST:', process.env.POSTGRES_HOST);
+// console.log('POSTGRES_PORT:', process.env.POSTGRES_PORT);
+// console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
+// console.log('POSTGRES_PASSWORD:', typeof process.env.POSTGRES_PASSWORD, process.env.POSTGRES_PASSWORD);
+// console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
+// console.log('----------------------');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
